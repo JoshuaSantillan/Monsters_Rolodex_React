428b8e22a7b844a5e5d4b8c21a8971af62aaee0a
Joshua  Santillan <joshuasantillan@Joshuas-MacBook-Pro.local>
2020-08-09 04:16:18 -0700

    Updates

---
+++ precache-manifest.411696c5cab1d7529294bb3dfec47c3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c397304c7f12846abec1fae685a04fd9",
+    "url": "/Monsters_Rolodex_React/index.html"
+  },
+  {
+    "revision": "f40b2fffdd231b99466c",
+    "url": "/Monsters_Rolodex_React/static/css/main.c84ab5ce.chunk.css"
+  },
+  {
+    "revision": "5ceb71a0b3552c2613c8",
+    "url": "/Monsters_Rolodex_React/static/js/2.95da3331.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters_Rolodex_React/static/js/2.95da3331.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f40b2fffdd231b99466c",
+    "url": "/Monsters_Rolodex_React/static/js/main.721b64d7.chunk.js"
+  },
+  {
+    "revision": "8e53e45b3d1017f84fe2",
+    "url": "/Monsters_Rolodex_React/static/js/runtime-main.46d92821.js"
+  }
+]);
